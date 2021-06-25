0a883f6950d504cc88fe5a08dbecba56cd34e77d
Torin Hawkins <torincyie13@gmail.com>
2021-06-24 19:05:42 -0500

    Add project files.

---
+++ SQLTester/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace SQLTester
+{
+    partial class frmSQLTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdSQLTester = new System.Windows.Forms.DataGridView();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRecords = new System.Windows.Forms.Label();
+            this.txtSQLTester = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grdSQLTester)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdSQLTester
+            // 
+            this.grdSQLTester.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdSQLTester.Location = new System.Drawing.Point(13, 13);
+            this.grdSQLTester.Name = "grdSQLTester";
+            this.grdSQLTester.Size = new System.Drawing.Size(775, 325);
+            this.grdSQLTester.TabIndex = 0;
+            this.grdSQLTester.TabStop = false;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(628, 355);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(159, 30);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Test SQL Statement";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(628, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 44);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Records Returned";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblRecords
+            // 
+            this.lblRecords.BackColor = System.Drawing.Color.White;
+            this.lblRecords.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRecords.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecords.Location = new System.Drawing.Point(688, 388);
+            this.lblRecords.Name = "lblRecords";
+            this.lblRecords.Size = new System.Drawing.Size(100, 50);
+            this.lblRecords.TabIndex = 3;
+            this.lblRecords.Text = "0";
+            this.lblRecords.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSQLTester
+            // 
+            this.txtSQLTester.Location = new System.Drawing.Point(13, 355);
+            this.txtSQLTester.Multiline = true;
+            this.txtSQLTester.Name = "txtSQLTester";
+            this.txtSQLTester.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSQLTester.Size = new System.Drawing.Size(609, 83);
+            this.txtSQLTester.TabIndex = 4;
+            // 
+            // frmSQLTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSQLTester);
+            this.Controls.Add(this.lblRecords);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.grdSQLTester);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmSQLTester";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Tester";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmSQLTester_FormClosing);
+            this.Load += new System.EventHandler(this.frmSQLTester_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdSQLTester)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdSQLTester;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRecords;
+        private System.Windows.Forms.TextBox txtSQLTester;
+    }
+}
+
---
+++ SQLTester/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace SQLTester
+{
+    public partial class frmSQLTester : Form
+    {
+        public frmSQLTester()
+        {
+            InitializeComponent();
+        }
+        SqlConnection booksConnection;
+
+        private void frmSQLTester_Load(object sender, EventArgs e)
+        {
+            string path = Path.GetFullPath("SQLBooksDB.mdf");
+            // connect to books database
+            booksConnection = new
+                SqlConnection("Data Source=.\\SQLEXPRESS; AttachDBFilename=C:\\Users\\thawkins022713\\source\\repos\\SQLTester\\SQLTester\\SQLBooksDB.mdf;" +
+                "Integrated Security=True; Connect Timeout=30; User Instance=True");
+            booksConnection.Open();
+        }
+
+        private void frmSQLTester_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            booksConnection.Close();
+            booksConnection.Dispose();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            SqlCommand resultsCommand = null;
+            SqlDataAdapter resultsAdapter = new
+                SqlDataAdapter();
+            DataTable resultsTable = new DataTable();
+            try
+            {
+                // establish command object and data table
+                resultsCommand = new SqlCommand(txtSQLTester.Text, booksConnection);
+                resultsAdapter.SelectCommand = resultsCommand;
+                resultsAdapter.Fill(resultsTable);
+                // bind grid view to data table
+                grdSQLTester.DataSource = resultsTable;
+                lblRecords.Text = resultsTable.Rows.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in Processing SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            resultsCommand.Dispose();
+            resultsAdapter.Dispose();
+            resultsTable.Dispose();
+        }
+    }
+}
